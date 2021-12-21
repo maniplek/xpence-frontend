dfafb2a31b4c62f8eae8bdd84fc16ab4fc758b3b
Maniple Kwizera <83705949+maniplek@users.noreply.github.com>
2021-12-21 10:00:07 +0200

    mentioned the exactly main route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="App">
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Signup />
             </Route>
             <Route path="/Login">
