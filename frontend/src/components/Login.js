666f90b2938fbc38c78f0f855c24ef93adef08cd
Maniple Kwizera <83705949+maniplek@users.noreply.github.com>
2021-12-21 15:28:06 +0200

    added hooks in login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,33 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../assets/XPENCE.svg";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [userInfo, setUserInfo] =useState([]);
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+    const newEntry = { email , password}
+
+    setUserInfo([ ...userInfo, newEntry])
+    console.log(userInfo);
+  }
+
   return (
     <div className="logi-page">
       <img src={img} alt="logo"></img>
-      <form action="">
+      <form action="" onSubmit={submitLogin}>
         <h2>Welcome to XPENCE</h2>
         <h4>Login to continue</h4>
         <div>
-            <label for="name">Name</label>
-            <br />
-            <input type='name' name='name' autoComplete='off' placeholder="Enter Name"></input>
+          <label for="email">Email</label>
+          <br />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="off"
+            placeholder="Enter Email"
+            required="required"
+          ></input>
         </div>
         <div>
-            <label for="password">Password</label>
-            <br />
-            <input type='password' name='password' autoComplete='off' placeholder="Password"></input>
+          <label for="password">Password</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="off"
+            placeholder="Password"
+            required
+          ></input>
         </div>
         <di>
-          <button type='submit'>Login</button>
+          <button type="submit">Login</button>
         </di>
 
         <div>
-            <h4>Forgot password?</h4>
+          <h4>Forgot password?</h4>
         </div>
       </form>
 
-        <h4>TaskForce 5.0</h4>
+      <h4>TaskForce 5.0</h4>
     </div>
   );
 }
